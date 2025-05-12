82895ccf49964754a498aa016b86dc1618272be7
Fabrizzio Pereira <fabrizzioper@gmail.com>
2025-05-11 19:54:04 -0500

    style: format next.config.mjs for consistency and readability

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   output: 'export',      
+  output: 'export',
   trailingSlash: true,
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
-
-module.exports = nextConfig
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
+};
+module.exports = nextConfig;
